299f7218bf21630ebe9f475146e3e9e558568050
KirSanHP <DieselHP@mail.ru>
2020-01-16 22:58:21 +0300

    Update LED_DISPLAY_2digits.ino
    Now increasing by one button, decreasing by another
    Added button ignore time (anti-bounce)

--- LED_DISPLAY_2digits.ino
+++ LED_DISPLAY_2digits.ino
@@ -1,7 +1,18 @@
-//Скетч должен выводить две цифры, сменяемые по нажатию кнопок в порядке возрастания
+//Скетч должен выводить две цифры, сменяемые по нажатию кнопок в порядке возрастания или убывания
 //методом записи байта в порт D
+// добавлено время игнорирования кнопки
+// добавлено долгое нажатие с приращением или убавлением числа пока кнопка удерживается
+// если нажаты обе кнопки одновременно, устанавливаются нули
 
-#define DOT_TIME 300
+//скетч работает нестабильно, как-то по настроению, сохранён ради интереса
+
+
+#define DOT_TIME 300        //интервал мигания точки
+#define BTN_IGNORE_TIME 100 //время игнорирования кнопки на время дребезга
+#define SPEED1TIME 500      //время до начала прибавления числа при нажатой кнопке
+#define SPEED2TIME 1500     //время до перехода на повышенную скорость изменения числа при нажатой кнопке
+#define SPEED1MILLIS 200    //миллисекунды первой скорости
+#define SPEED2MILLIS 100    //миллисекунды второй скорости
 
 #define ZERO  B01000000
 #define ONE   B01111001
@@ -21,15 +32,28 @@
 #define BTN1_PIN 14 //A0
 #define BTN2_PIN 15 //A1
 
-bool btn1Prev = 0;
-bool btn2Prev = 0;
-byte digit1 = 0;
-byte digit2 = 0;
 bool dot1 = 0;
 bool dot2 = 0;
 unsigned long millisPrev = 0;
 unsigned long switchTime = 0;
 unsigned long dotPrevTime = 0;
+
+struct Num {
+  byte digit2;
+  byte digit1;
+  char change;
+};
+
+struct Btn {
+  bool currentState;
+  bool prevState;
+  unsigned long prevTime;
+  unsigned long pressTime;
+};
+
+Btn btn1 = {0, 0, 0, 0};
+Btn btn2 = {0, 0, 0, 0};
+Num num = {3, 6, 0};
 
 void setup() {
   pinMode (ANODE1, OUTPUT);
@@ -44,34 +68,27 @@ void setup() {
   millisPrev = millis();
   pinMode (BTN1_PIN, INPUT_PULLUP);
   pinMode (BTN2_PIN, INPUT_PULLUP);
-//  pinMode (DOT_PIN, OUTPUT);
   digitalWrite (ANODE1, HIGH);
 }
 
-int main() {
-  bool btn1 = !digitalRead (BTN1_PIN);
-  bool btn2 = !digitalRead (BTN2_PIN);
-  if (btn1 == 1 && btn1Prev == 0) {
-    digit1++;
-    if (digit1 == 10) {
-      digit1 = 0;
-      digit2++;
-      if (digit2 == 10)
-        digit2 = 0;
-    }
-  }
+void loop() {
+  btn1 = checkButton (BTN1_PIN, btn1);
+  btn2 = checkButton (BTN2_PIN, btn2);
 
-  if (btn2 == 1 && btn2Prev == 0) {
-    digit1--;
-    if (digit1 == 255) {
-      digit1 = 9;
-      digit2--;
-      if (digit2 == 255)
-        digit2 = 9;
-    }
+  if (btn1.currentState && btn2.currentState && (!btn1.prevState || !btn2.prevState) ) {
+    num = {0, 0, 0};
+    btn1.prevState = btn1.currentState;
+    btn2.prevState = btn2.currentState;
   }
-  btn1Prev = btn1;
-  btn2Prev = btn2;
+  else if (btn1.currentState && !btn1.prevState && !btn2.currentState) {
+    num.change = 1;
+    num = numCalc (num);
+    btn1.prevState = btn1.currentState;
+  }
+  else if (btn2.currentState && !btn2.prevState && !btn1.currentState) {
+    num.change = -1;
+    num = numCalc (num);
+  }
 
   if (millis() >= dotPrevTime + DOT_TIME) {
     dot1 = !dot1;
@@ -82,16 +99,44 @@ int main() {
     if (digitalRead (ANODE1) ) {
       digitalWrite (ANODE1, LOW);
       digitalWrite (ANODE2, HIGH);
-      digitOut (digit2, dot2);
+      digitOut (num.digit2, dot2);
     }
     else {
       digitalWrite (ANODE1, HIGH);
       digitalWrite (ANODE2, LOW);
-      digitOut (digit1, dot1);
+      digitOut (num.digit1, dot1);
     }
     switchTime += 5;
   }
-  return 0;
+delay (200);
+}
+
+Btn checkButton (byte _btnPin, Btn _btn) {
+  bool _btnState = !digitalRead (_btnPin);
+  unsigned long currentMillis = millis();
+  if ( _btnState && !_btn.prevState && ( currentMillis >= _btn.prevTime + BTN_IGNORE_TIME) )
+    return {1, _btn.prevState, currentMillis, 0};
+  else if (!_btnState && _btn.prevState && ( currentMillis >= _btn.prevTime + BTN_IGNORE_TIME) ) {
+    return {0, _btn.prevState, _btn.prevTime, (currentMillis - _btn.prevTime) };
+  }
+}
+
+Num numCalc(Num _num) {
+  _num.digit1 += _num.change;
+  if (_num.digit1 == 10) {
+    _num.digit1 = 0;
+    _num.digit2++;
+    if (_num.digit2 == 10)
+      _num.digit2 = 0;
+  }
+  else if (num.digit1 == 255) {
+    _num.digit1 = 9;
+    _num.digit2 --;
+    if (_num.digit2 == 255)
+      _num.digit2 = 9;
+  }
+  _num.change = 0;
+  return {_num.digit2, _num.digit1, _num.change};
 }
 
 void digitOut (byte _digit, bool _dot) {
